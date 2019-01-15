a0fb378b154c61e2f11795eb8c8cb678e92f5764
acaoer <lycbuct@gmail.com>
2019-01-15 11:02:42 +0800

    Updates

---
+++ precache-manifest.23a07f1a7cf671ad00a0b227fae7bf68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "7040172c800a09251008",
+    "url": "/react-tutorial/static/js/main.7040172c.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-tutorial/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "7040172c800a09251008",
+    "url": "/react-tutorial/static/css/main.6b287aef.chunk.css"
+  },
+  {
+    "revision": "601ace91851541072f67b45fc696ec46",
+    "url": "/react-tutorial/index.html"
+  }
+];
